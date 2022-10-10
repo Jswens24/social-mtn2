240ffb0fc7f90987963152a55ce94844fffccb44
Jessica Swenson <jessicamswens@gmail.com>
2022-10-09 19:59:47 -0600

    started server

---
+++ server/controllers/posts.js
@@ -0,0 +1,17 @@
+module.exports = {
+    getAllPosts: (req, res) => {
+
+    },
+    getCurrentUserPosts: (req, res) => {
+
+    },
+    addPost: (req, res) => {
+
+    },
+    editPost: (req, res) => {
+
+    },
+    deletePost: (req, res) => {
+
+    },
+}
