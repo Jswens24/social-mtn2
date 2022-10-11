b4eaeb667cdf543093fbbe24c62c5d5e47dc759c
Jessica Swenson <jessicamswens@gmail.com>
2022-10-10 20:11:16 -0600

    updating post features

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,52 @@
+const { Post } = require('../models/post');
+const { User } = require('../models/user');
+
 module.exports = {
-    getAllPosts: (req, res) => {
-
+    getAllPosts: async (req, res) => {
+        try {
+            const posts = await Post.findAll({
+                where: { privateStatus: false },
+                include: [{
+                    model: User,
+                    required: true,
+                    attributes: [`username`]
+                }]
+            })
+            res.status(200).send(posts)
+        } catch (error) {
+            console.log('ERROR IN getAllPosts')
+            console.log(error)
+            res.sendStatus(400)
+        }
     },
-    getCurrentUserPosts: (req, res) => {
-
+    getCurrentUserPosts: async (req, res) => {
+        try {
+            const { userId } = req.params
+            const posts = await Post.findAll({
+                where: { userId: userId },
+                include: [{
+                    model: User,
+                    required: true,
+                    attributes: [`username`]
+                }]
+            })
+            res.status(200).send(posts)
+        } catch (error) {
+            console.log('ERROR IN getCurrentUserPosts')
+            console.log(error)
+            res.sendStatus(400)
+        }
     },
-    addPost: (req, res) => {
-
+    addPost: async (req, res) => {
+        try {
+            const { title, content, status, userId } = req.body
+            await Post.create({ title, content, privateStatus: status, userId })
+            res.sendStatus(200)
+        } catch (error) {
+            console.log('ERROR IN getCurrentUserPosts')
+            console.log(error)
+            res.sendStatus(400)
+        }
     },
     editPost: (req, res) => {
 
